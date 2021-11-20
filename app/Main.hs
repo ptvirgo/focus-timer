f7d1ba2398b933464f069417ab4cc768d857d38f
Pablo Virgo <mailbox@pablovirgo.com>
2021-11-20 15:08:24 -0500

    Text entry for task...

--- app/Main.hs
+++ app/Main.hs
@@ -7,37 +7,60 @@ import Control.Monad ( void )
 import Control.Concurrent ( threadDelay )
 import Data.Functor ( ($>) )
 import Data.Text as Text
+import Data.String ( IsString (..) )
 import Pipes.Prelude ( repeatM )
 
 import GI.Gtk ( Box (..)
               , Button (..)
+              , Entry (..)
               , Label (..)
               , Window (..)
               , Orientation (..)
               )
 
+import GI.Gtk( entryGetText )
 import GI.Gtk.Declarative
 import GI.Gtk.Declarative.App.Simple
 
-data State =
-  State { seconds :: Integer }
-
-data Event = Tick | Quit
 
 oneSecond :: Int
 oneSecond = 1000000
 
+minute :: Int
+minute = 60
+
+workTime :: Int
+workTime = minute * 20
+
+breakTime :: Int
+breakTime = minute * 5
+
+newtype Task = Task Text.Text
+
+instance Show Task where
+    show (Task t) = Text.unpack t
+
+instance IsString Task where
+    fromString s = Task . Text.pack $ s
+
+data State = WhatsNext Task
+data Event = StartWorking Task | UpdateTask Task | Tick | Quit
+
 update' :: State -> Event -> Transition State Event
-update' s Tick = Transition (s { seconds = seconds s + 1 } ) (return Nothing)
+update' (WhatsNext _) (UpdateTask task) = Transition (WhatsNext task) (return Nothing)
 update' _ Quit = Exit
+update' s _ = Transition s (return Nothing)
+-- update' s Tick = Transition (s { seconds = seconds s + 1 } ) (return Nothing)
 
 view' :: State -> AppView Window Event
-view' s = bin Window
-  [ #title := "Focus Timer"
-  , on #deleteEvent (const (True, Quit))
-  ]
-  $ container Box [ #orientation := OrientationVertical ]
-  [ widget Label [ #label := ( Text.pack . show . seconds $ s ) ]
+view' s = bin Window [ #title := "Focus Timer" , on #deleteEvent (const (True, Quit)) ] appState where
+    appState = case s of
+        WhatsNext task -> viewWhatsNext task
+
+viewWhatsNext :: Task -> Widget Event
+viewWhatsNext task = container Box [ #orientation := OrientationVertical ]
+  [ widget Label [ #label := "What's next?" ]
+  , widget Entry [ onM #changed (\evBox -> UpdateTask . Task <$> entryGetText evBox) ]
   , widget Button [#label := "Quit", on #clicked Quit]
   ]
 
@@ -47,6 +70,6 @@ main :: IO ()
 main = void $ run App { view = view'
                , update = update'
                , inputs = [timer]
-               , initialState = State { seconds = 0 }
+               , initialState = WhatsNext ""
                }
 
