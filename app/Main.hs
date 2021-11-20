258c7709f3e6b53544b889561166ed09d61b5a22
Pablo Virgo <mailbox@pablovirgo.com>
2021-11-20 13:15:46 -0500

    how to tick

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,15 @@ module Main where
 
 import Control.Monad ( void )
 import Control.Concurrent ( threadDelay )
-import Data.Text
+import Data.Functor ( ($>) )
+import Data.Text as Text
+import Pipes.Prelude ( repeatM )
 
 import GI.Gtk ( Box (..)
               , Button (..)
               , Label (..)
               , Window (..)
+              , Orientation (..)
               )
 
 import GI.Gtk.Declarative
@@ -21,8 +24,11 @@ data State =
 
 data Event = Tick | Quit
 
+oneSecond :: Int
+oneSecond = 1000000
+
 update' :: State -> Event -> Transition State Event
-update' (State s) Tick = Transition (State s) (return Nothing)
+update' s Tick = Transition (s { seconds = seconds s + 1 } ) (return Nothing)
 update' _ Quit = Exit
 
 view' :: State -> AppView Window Event
@@ -30,13 +36,17 @@ view' s = bin Window
   [ #title := "Focus Timer"
   , on #deleteEvent (const (True, Quit))
   ]
-  $ container Box []
-  [ widget Button [#label := "Quit", on #clicked Quit]
+  $ container Box [ #orientation := OrientationVertical ]
+  [ widget Label [ #label := ( Text.pack . show . seconds $ s ) ]
+  , widget Button [#label := "Quit", on #clicked Quit]
   ]
+
+timer = repeatM $ threadDelay oneSecond $> Tick
 
 main :: IO ()
 main = void $ run App { view = view'
                , update = update'
-               , inputs = []
+               , inputs = [timer]
                , initialState = State { seconds = 0 }
                }
+
